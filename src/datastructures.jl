091acb79038d0e63fbcb4d54fe4bd8e9f65bfe26
Unknown <louisponet@gmail.com>
2020-09-07 10:31:10 +0200

    PackedIntSet and Overseer exist so removed them

--- src/datastructures.jl
+++ src/datastructures.jl
@@ -1,15 +1,11 @@
 module DataStructures
 	include("datastructures/TypedPage.jl")
-	include("datastructures/PackedIntSet.jl")
 	include("datastructures/GappedVector.jl")
 	include("datastructures/LooseVector.jl")
 	export TypedPage
-	export PackedIntSet
 	export packed_id
 	export GappedVector
 	export LooseVector
-	export pointer_zip
-	export cleanup!
 
 	const PAGESIZE = 0
 	function __init__()
